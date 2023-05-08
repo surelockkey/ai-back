71f1844c196ff52436b666786013d306dc207ad9
Bohdan Velykodnyi <bohdanvelykodnyi@Bohdans-MacBook-Pro.local>
2023-05-08 17:49:23 +0300

    fix 13

--- src/modules/api/google-sheets-api/google-sheets-api.service.ts
+++ src/modules/api/google-sheets-api/google-sheets-api.service.ts
@@ -1,45 +1,43 @@
 import { Injectable } from '@nestjs/common';
-// import { GoogleAuth } from 'google-auth-library';
-// import { sheets } from 'googleapis/build/src/apis/sheets';
-import {google} from 'googleapis';
-
+import { GoogleAuth } from 'google-auth-library';
+import { sheets } from 'googleapis/build/src/apis/sheets';
 
 @Injectable()
 export class GoogleSheetsApiService {
-  // private readonly auth;
-  // private google_sheets;
+  private readonly auth;
+  private google_sheets;
 
-  // constructor() {
-  //   this.auth = new GoogleAuth({
-  //     keyFile: 'liquid-folio-385713-8f81f48d5101.json',
-  //     scopes: 'https://www.googleapis.com/auth/spreadsheets',
-  //   });
+  constructor() {
+    this.auth = new GoogleAuth({
+      keyFile: 'liquid-folio-385713-8f81f48d5101.json',
+      scopes: 'https://www.googleapis.com/auth/spreadsheets',
+    });
 
-  //   this.loadAsync();
-  // }
+    this.loadAsync();
+  }
 
-  // private async loadAsync() {
-  //   const auth_client = await this.auth.getClient();
+  private async loadAsync() {
+    const auth_client = await this.auth.getClient();
 
-  //   this.google_sheets = sheets({ version: 'v4', auth: auth_client });
-  // }
+    this.google_sheets = sheets({ version: 'v4', auth: auth_client });
+  }
 
-  // public async getSheet(
-  //   sheet_id: string,
-  //   tab_name: string,
-  //   col: string,
-  //   row: string,
-  // ) {
-  //   // const sheet_id = '1zP51f9-mJ-Pfkl0DhJZow54A_ux5YTCvx0Y2i9BjlyQ';
+  public async getSheet(
+    sheet_id: string,
+    tab_name: string,
+    col: string,
+    row: string,
+  ) {
+    // const sheet_id = '1zP51f9-mJ-Pfkl0DhJZow54A_ux5YTCvx0Y2i9BjlyQ';
 
-  //   const res = await this.google_sheets.spreadsheets.values.get({
-  //     auth: this.auth,
-  //     spreadsheetId: sheet_id,
-  //     range: `${tab_name}!${col}:${row}`,
-  //   });
+    const res = await this.google_sheets.spreadsheets.values.get({
+      auth: this.auth,
+      spreadsheetId: sheet_id,
+      range: `${tab_name}!${col}:${row}`,
+    });
 
-  //   console.log(res.data);
+    console.log(res.data);
 
-  //   return res.data;
-  // }
+    return res.data;
+  }
 }
