a941c171d58c37b4a7b8e1eaa48170f6f8c233f9
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2023-05-12 17:17:06 +0300

    fix

--- src/modules/authorization/decorator/role.decorator.ts
+++ src/modules/authorization/decorator/role.decorator.ts
@@ -1,6 +1,6 @@
 import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
-import { GqlAuthGuard } from '../Guard/auth.guard';
-import { CheckRoleGuard } from '../Guard/role.guard';
+import { GqlAuthGuard } from '../guard/auth.guard';
+import { CheckRoleGuard } from '../guard/role.guard';
 import { UserRole } from 'src/modules/user/enum/user-role.enum';
 
 export function RoleGuard(...roles: UserRole[]) {
