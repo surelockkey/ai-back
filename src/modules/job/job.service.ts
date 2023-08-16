04452e6cb257c0bc8403032ea6e2084a254fcbbf
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2023-08-16 13:24:57 +0300

    fix

--- src/modules/job/job.service.ts
+++ src/modules/job/job.service.ts
@@ -24,23 +24,11 @@ export class JobService extends CrudService<Job> {
   }
 
   public async jobLoop() {
-    // await this.jobRepository
-    //   .createQueryBuilder('job')
-    //   .delete()
-    //   .from(Job)
-    //   .execute();
+    await this.jobRepository.delete({ account: 'arizona' });
 
-    // await this.callRepository
-    //   .createQueryBuilder('call')
-    //   .delete()
-    //   .from(Call)
-    //   .execute();
+    await this.callRepository.delete({ account: 'arizona' });
 
-    // await this.activityLogRepository
-    //   .createQueryBuilder('activityLog')
-    //   .delete()
-    //   .from(ActivityLog)
-    //   .execute();
+    await this.activityLogRepository.delete({ account: 'arizona' });
 
     const current_date = moment();
     let year = 18;
@@ -149,79 +137,81 @@ export class JobService extends CrudService<Job> {
 
       let activity_counter = 0;
 
-      while (activity_counter < activities.length) {
-        if (activities[activity_counter]._type === 'calls') {
-          await this.callRepository.save({
-            call_id: activities[activity_counter].id,
-            call_sid: activities[activity_counter].call_sid,
-            created: activities[activity_counter].created,
-            from: activities[activity_counter].from,
-            to: activities[activity_counter].to,
-            recording_url: activities[activity_counter].recording_url,
-            from_zip: activities[activity_counter].from_zip,
-            to_zip: activities[activity_counter].to_zip,
-            call_duration: activities[activity_counter].call_duration,
-            call_status: activities[activity_counter].call_status,
-            direction: activities[activity_counter].direction,
-            job_id: activities[activity_counter].job_id,
-            is_active: activities[activity_counter].is_active,
-            client_id: activities[activity_counter].client_id,
-            ad_group_id: activities[activity_counter].ad_group_id,
-            phoneNumber: activities[activity_counter].phoneNumber,
-            timeInt: activities[activity_counter].timeInt,
-            time: activities[activity_counter].time,
-            account: 'arizona',
-          });
-        } else {
-          await this.activityLogRepository.save({
-            activity_id: activities[activity_counter].id,
-            account_id: activities[activity_counter].account_id,
-            text: activities[activity_counter].text,
-            uid: activities[activity_counter].uid,
-            uname: activities[activity_counter].uname,
-            job_id: activities[activity_counter].job_id,
-            uuid: activities[activity_counter].uuid,
-            timestamp: activities[activity_counter].timestamp,
-            timeInt: activities[activity_counter].timeInt,
-            time: activities[activity_counter].time,
-            searchTerm: activities[activity_counter].searchTerm,
-            account: 'arizona',
-          });
+      if (job.data && activities) {
+        while (activity_counter < activities.length) {
+          if (activities[activity_counter]._type === 'calls') {
+            await this.callRepository.save({
+              call_id: activities[activity_counter].id,
+              call_sid: activities[activity_counter].call_sid,
+              created: activities[activity_counter].created,
+              from: activities[activity_counter].from,
+              to: activities[activity_counter].to,
+              recording_url: activities[activity_counter].recording_url,
+              from_zip: activities[activity_counter].from_zip,
+              to_zip: activities[activity_counter].to_zip,
+              call_duration: activities[activity_counter].call_duration,
+              call_status: activities[activity_counter].call_status,
+              direction: activities[activity_counter].direction,
+              job_id: activities[activity_counter].job_id,
+              is_active: activities[activity_counter].is_active,
+              client_id: activities[activity_counter].client_id,
+              ad_group_id: activities[activity_counter].ad_group_id,
+              phoneNumber: activities[activity_counter].phoneNumber,
+              timeInt: activities[activity_counter].timeInt,
+              time: activities[activity_counter].time,
+              account: 'arizona',
+            });
+          } else {
+            await this.activityLogRepository.save({
+              activity_id: activities[activity_counter].id,
+              account_id: activities[activity_counter].account_id,
+              text: activities[activity_counter].text,
+              uid: activities[activity_counter].uid,
+              uname: activities[activity_counter].uname,
+              job_id: activities[activity_counter].job_id,
+              uuid: activities[activity_counter].uuid,
+              timestamp: activities[activity_counter].timestamp,
+              timeInt: activities[activity_counter].timeInt,
+              time: activities[activity_counter].time,
+              searchTerm: activities[activity_counter].searchTerm,
+              account: 'arizona',
+            });
+          }
+
+          activity_counter++;
         }
 
-        activity_counter++;
+        await this.jobRepository.save({
+          uuid: job.data.uuid,
+          start_date: job.data.job_date,
+          end_date: job.data.job_end_date,
+          created_date: job.data.created,
+          total_price: Number(job.data.job_total_price),
+          amount_due: Number(job.data.job_amount_due),
+          client_id: Number(job.data.client_id),
+          status: job.data.status,
+          phone: job.data.primary_phone,
+          second_phone: job.data.secondary_phone,
+          email: job.data.client_email_address,
+          client_name: job.data.first_name + ' ' + job.data.last_name,
+          city: job.data.city,
+          state: job.data.state,
+          postal_code: job.data.zipcode,
+          job_type: job.data.type_name,
+          job_note: job.data.job_description,
+          job_source: job.data.job_source,
+          technician_name: job.data.techs[0] && job.data.techs[0].technition,
+          dispatcher_name: job.data.user_created,
+          address: job.data.address,
+          tip_amount: job.data.tip_amount,
+          job_timezone: job.data.job_timezone,
+          tax_amount: job.data.tax_amount,
+          tax_precent: job.data.tax_precent,
+          job_id: job.data.job_id,
+          avg_duration: job.data.avg_duration,
+          account: 'arizona',
+        });
       }
-
-      await this.jobRepository.save({
-        uuid: job.data.uuid,
-        start_date: job.data.job_date,
-        end_date: job.data.job_end_date,
-        created_date: job.data.created,
-        total_price: Number(job.data.job_total_price),
-        amount_due: Number(job.data.job_amount_due),
-        client_id: Number(job.data.client_id),
-        status: job.data.status,
-        phone: job.data.primary_phone,
-        second_phone: job.data.secondary_phone,
-        email: job.data.client_email_address,
-        client_name: job.data.first_name + ' ' + job.data.last_name,
-        city: job.data.city,
-        state: job.data.state,
-        postal_code: job.data.zipcode,
-        job_type: job.data.type_name,
-        job_note: job.data.job_description,
-        job_source: job.data.job_source,
-        technician_name: job.data.techs[0] && job.data.techs[0].technition,
-        dispatcher_name: job.data.user_created,
-        address: job.data.address,
-        tip_amount: job.data.tip_amount,
-        job_timezone: job.data.job_timezone,
-        tax_amount: job.data.tax_amount,
-        tax_precent: job.data.tax_precent,
-        job_id: job.data.job_id,
-        avg_duration: job.data.avg_duration,
-        account: 'arizona',
-      });
     } catch (e) {
       console.log(e);
     }
