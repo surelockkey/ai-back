257eb105b28f54546deba2d4f796b938acd71d15
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2023-10-09 11:07:11 +0300

    fix

--- src/modules/job/job.service.ts
+++ src/modules/job/job.service.ts
@@ -85,11 +85,15 @@ export class JobService {
       const req = await this.getJobsByRange(current_page, year, month, account);
       const jobs = req.aaData;
 
-      await Promise.all(
-        (jobs || [])?.map(async (job) => {
-          await this.getFullJob(job.uuid, account);
-        }),
-      );
+      // await Promise.all(
+      //   (jobs || [])?.map(async (job) => {
+      //     await this.getFullJob(job.uuid, account);
+      //   }),
+      // );
+
+      for (const job of jobs) {
+        await this.getFullJob(job.uuid, account);
+      }
 
       current_page++;
     }
@@ -145,82 +149,16 @@ export class JobService {
     }
   }
 
-  private async getFullJob(
-    job_id: string,
-    account?: 'main' | 'arizona',
-    count = 0,
-  ) {
+  private async getFullJob(job_id: string, account?: 'main' | 'arizona') {
     try {
-      const job = await this.workizCoreApiService
-        .req(`/ajaxc/job/get/${job_id}/`, 'post', undefined, account)
-        .catch(async (e) => {
-          // console.log(e);
-        })
-        .then((r) => {
-          if (r) {
-            return r;
-          }
-        });
-
-      // if (!job.data || !job.data?.job_id) {
-      // console.log(job);
-      // }
-
-      const activities = (
-        await this.workizCoreApiService.req(
-          `/ajaxc/job/jobTimelineFull/${job.data.job_id}/`,
-          'post',
-          {},
-          account,
-        )
-      ).data;
-
-      // let activity_counter = 0;
+      const job = await this.workizCoreApiService.req(
+        `/ajaxc/job/get/${job_id}/`,
+        'post',
+        undefined,
+        account,
+      );
 
       if (job.data) {
-        // while (activity_counter < activities.length) {
-        //   // if (activities[activity_counter]._type === 'calls') {
-        //   //   // await this.callRepository.save({
-        //   //   //   call_id: activities[activity_counter].id,
-        //   //   //   call_sid: activities[activity_counter].call_sid,
-        //   //   //   created: activities[activity_counter].created,
-        //   //   //   from: activities[activity_counter].from,
-        //   //   //   to: activities[activity_counter].to,
-        //   //   //   recording_url: activities[activity_counter].recording_url,
-        //   //   //   from_zip: activities[activity_counter].from_zip,
-        //   //   //   to_zip: activities[activity_counter].to_zip,
-        //   //   //   call_duration: activities[activity_counter].call_duration,
-        //   //   //   call_status: activities[activity_counter].call_status,
-        //   //   //   direction: activities[activity_counter].direction,
-        //   //   //   job_id: activities[activity_counter].job_id,
-        //   //   //   is_active: activities[activity_counter].is_active,
-        //   //   //   client_id: activities[activity_counter].client_id,
-        //   //   //   ad_group_id: activities[activity_counter].ad_group_id,
-        //   //   //   phoneNumber: activities[activity_counter].phoneNumber,
-        //   //   //   timeInt: activities[activity_counter].timeInt,
-        //   //   //   time: activities[activity_counter].time,
-        //   //   //   account,
-        //   //   // });
-        //   // } else {
-        //   //   // await this.activityLogRepository.save({
-        //   //   //   activity_id: activities[activity_counter].id,
-        //   //   //   account_id: activities[activity_counter].account_id,
-        //   //   //   text: activities[activity_counter].text,
-        //   //   //   uid: activities[activity_counter].uid,
-        //   //   //   uname: activities[activity_counter].uname,
-        //   //   //   job_id: activities[activity_counter].job_id,
-        //   //   //   uuid: activities[activity_counter].uuid,
-        //   //   //   timestamp: activities[activity_counter].timestamp,
-        //   //   //   timeInt: activities[activity_counter].timeInt,
-        //   //   //   time: activities[activity_counter].time,
-        //   //   //   searchTerm: activities[activity_counter].searchTerm,
-        //   //   //   account: 'arizona',
-        //   //   // });
-        //   // }
-
-        //   activity_counter++;
-        // }
-
         const countie = await this.countieService.findOneWithoutError({
           city: job?.data?.city,
           state: job?.data?.state,
