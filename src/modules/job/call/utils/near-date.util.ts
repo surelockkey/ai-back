1b4808e0686bedbb415e87727c2362a254445526
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2023-10-18 15:32:18 +0300

    fix

--- src/modules/job/call/utils/near-date.util.ts
+++ src/modules/job/call/utils/near-date.util.ts
@@ -7,10 +7,10 @@ export function getNearestDate(
   let nearestDate;
 
   datesToBeChecked.forEach((date) => {
-    const diff = moment(date).diff(moment(dateToCheckFor), 'days');
+    const diff = moment(date).utc().diff(moment(dateToCheckFor), 'days');
     if (diff > 0) {
       if (nearestDate) {
-        if (moment(date).diff(moment(nearestDate), 'days') < 0) {
+        if (moment(date).utc().diff(moment(nearestDate), 'days') < 0) {
           nearestDate = date;
         }
       } else {
