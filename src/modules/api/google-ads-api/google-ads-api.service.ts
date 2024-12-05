a02af7311124056483266b66924f8c114f0b13b8
Roman <roman.senyshyn@surelockkey.com>
2024-12-05 15:18:43 +0200

    update Ad service

--- src/modules/api/google-ads-api/google-ads-api.service.ts
+++ src/modules/api/google-ads-api/google-ads-api.service.ts
@@ -176,8 +176,8 @@ export class GoogleAdsApiService {
   public getAD = async (customer_id = process.env.GOOGLE_ADS_CUSTOMER_ID): Promise<AdDto[]> => {
     const customer = this.createCustomer(customer_id)
 
-    try {
-      const query = `
+    // try {
+    const query = `
         SELECT
           ad.added_by_google_ads,
           ad.app_ad.descriptions,
@@ -350,92 +350,92 @@ export class GoogleAdsApiService {
         LIMIT 50
       `;
 
-      const response = await customer.query(query);
+    const response = await customer.query(query);
 
-      console.log(response);
+    console.log(response);
 
 
-      return response.map(({ ad }) => ({
-        ad_id: ad.id,
-        type: enums.AdType[ad.type],
-        name: ad.name,
-        final_urls: ad.final_urls,
-        final_app_urls: ad.final_app_urls?.map((urls) => urls.url),
-        final_mobile_urls: ad.final_mobile_urls,
-        final_url_suffix: ad.final_url_suffix,
-        resource_name: ad.resource_name,
-        display_url: ad.display_url,
-        // description: ad.app_ad.descriptions,
-        // headline: ad.app_ad.headlines,
+    return response.map(({ ad }) => ({
+      ad_id: ad.id,
+      type: enums.AdType[ad.type],
+      name: ad.name,
+      final_urls: ad.final_urls,
+      final_app_urls: ad.final_app_urls?.map((urls) => urls.url),
+      final_mobile_urls: ad.final_mobile_urls,
+      final_url_suffix: ad.final_url_suffix,
+      resource_name: ad.resource_name,
+      display_url: ad.display_url,
+      // description: ad.app_ad.descriptions,
+      // headline: ad.app_ad.headlines,
 
-        // App ad details
-        app_ad_descriptions: ad.app_ad.descriptions?.map((asset) => asset.text),
-        app_ad_headlines: ad.app_ad.headlines?.map((asset) => asset.text),
-        app_ad_html5_media_bundles: ad.app_ad.html5_media_bundles?.map(bundle => bundle.asset),
-        app_ad_images: ad.app_ad.images?.map(image => image.asset),
-        app_ad_mandatory_ad_text: ad.app_ad.mandatory_ad_text.text,
-        app_ad_youtube_videos: ad.app_ad.youtube_videos?.map(video => video.asset),
+      // App ad details
+      app_ad_descriptions: ad.app_ad.descriptions?.map((asset) => asset.text),
+      app_ad_headlines: ad.app_ad.headlines?.map((asset) => asset.text),
+      app_ad_html5_media_bundles: ad.app_ad.html5_media_bundles?.map(bundle => bundle.asset),
+      app_ad_images: ad.app_ad.images?.map(image => image.asset),
+      app_ad_mandatory_ad_text: ad.app_ad.mandatory_ad_text.text,
+      app_ad_youtube_videos: ad.app_ad.youtube_videos?.map(video => video.asset),
 
-        // App Engagement Ad details
-        app_engagement_ad_descriptions: ad.app_engagement_ad.descriptions?.map(description => description.text),
-        app_engagement_ad_headlines: ad.app_engagement_ad.headlines?.map(headline => headline.text),
-        app_engagement_ad_images: ad.app_engagement_ad.images?.map(image => image.asset),
-        app_engagement_ad_videos: ad.app_engagement_ad.videos?.map(video => video.asset),
+      // App Engagement Ad details
+      app_engagement_ad_descriptions: ad.app_engagement_ad.descriptions?.map(description => description.text),
+      app_engagement_ad_headlines: ad.app_engagement_ad.headlines?.map(headline => headline.text),
+      app_engagement_ad_images: ad.app_engagement_ad.images?.map(image => image.asset),
+      app_engagement_ad_videos: ad.app_engagement_ad.videos?.map(video => video.asset),
 
-        // Call Ad details
-        call_ad_business_name: ad.call_ad.business_name,
-        call_ad_call_tracked: ad.call_ad.call_tracked,
-        call_ad_conversion_action: ad.call_ad.conversion_action,
-        call_ad_conversion_reporting_state: enums.CallConversionReportingState[ad.call_ad.conversion_reporting_state],
-        call_ad_country_code: ad.call_ad.country_code,
-        call_ad_description1: ad.call_ad.description1,
-        call_ad_description2: ad.call_ad.description2,
-        call_ad_disable_call_conversion: ad.call_ad.disable_call_conversion,
-        call_ad_headline1: ad.call_ad.headline1,
-        call_ad_headline2: ad.call_ad.headline2,
-        call_ad_path1: ad.call_ad.path1,
-        call_ad_path2: ad.call_ad.path2,
-        call_ad_phone_number: ad.call_ad.phone_number,
-        call_ad_phone_number_verification_url: ad.call_ad.phone_number_verification_url,
+      // Call Ad details
+      call_ad_business_name: ad.call_ad.business_name,
+      call_ad_call_tracked: ad.call_ad.call_tracked,
+      call_ad_conversion_action: ad.call_ad.conversion_action,
+      call_ad_conversion_reporting_state: enums.CallConversionReportingState[ad.call_ad.conversion_reporting_state],
+      call_ad_country_code: ad.call_ad.country_code,
+      call_ad_description1: ad.call_ad.description1,
+      call_ad_description2: ad.call_ad.description2,
+      call_ad_disable_call_conversion: ad.call_ad.disable_call_conversion,
+      call_ad_headline1: ad.call_ad.headline1,
+      call_ad_headline2: ad.call_ad.headline2,
+      call_ad_path1: ad.call_ad.path1,
+      call_ad_path2: ad.call_ad.path2,
+      call_ad_phone_number: ad.call_ad.phone_number,
+      call_ad_phone_number_verification_url: ad.call_ad.phone_number_verification_url,
 
-        // Demand Gen Carousel Ad details
-        demand_gen_carousel_ad_business_name: ad.demand_gen_carousel_ad.business_name,
-        demand_gen_carousel_ad_call_to_action_text: ad.demand_gen_carousel_ad.call_to_action_text,
-        demand_gen_carousel_ad_carousel_cards: ad.demand_gen_carousel_ad.carousel_cards?.map(card => card.asset),
-        demand_gen_carousel_ad_description: ad.demand_gen_carousel_ad.description.text,
-        demand_gen_carousel_ad_headline: ad.demand_gen_carousel_ad.headline.text,
-        demand_gen_carousel_ad_logo_image: ad.demand_gen_carousel_ad.logo_image.asset,
+      // Demand Gen Carousel Ad details
+      demand_gen_carousel_ad_business_name: ad.demand_gen_carousel_ad.business_name,
+      demand_gen_carousel_ad_call_to_action_text: ad.demand_gen_carousel_ad.call_to_action_text,
+      demand_gen_carousel_ad_carousel_cards: ad.demand_gen_carousel_ad.carousel_cards?.map(card => card.asset),
+      demand_gen_carousel_ad_description: ad.demand_gen_carousel_ad.description.text,
+      demand_gen_carousel_ad_headline: ad.demand_gen_carousel_ad.headline.text,
+      demand_gen_carousel_ad_logo_image: ad.demand_gen_carousel_ad.logo_image.asset,
 
-        // Other demand gen details (multi-asset, product, video responsive ads)
-        demand_gen_multi_asset_ad_business_name: ad.demand_gen_multi_asset_ad.business_name,
-        demand_gen_multi_asset_ad_call_to_action_text: ad.demand_gen_multi_asset_ad.call_to_action_text,
-        demand_gen_multi_asset_ad_descriptions: ad.demand_gen_multi_asset_ad.descriptions?.map(description => description.text),
-        demand_gen_multi_asset_ad_headlines: ad.demand_gen_multi_asset_ad.headlines?.map(headline => headline.text),
-        demand_gen_multi_asset_ad_lead_form_only: ad.demand_gen_multi_asset_ad.lead_form_only,
-        demand_gen_multi_asset_ad_logo_images: ad.demand_gen_multi_asset_ad.logo_images?.map(image => image.asset),
-        demand_gen_multi_asset_ad_marketing_images: ad.demand_gen_multi_asset_ad.marketing_images?.map(image => image.asset),
+      // Other demand gen details (multi-asset, product, video responsive ads)
+      demand_gen_multi_asset_ad_business_name: ad.demand_gen_multi_asset_ad.business_name,
+      demand_gen_multi_asset_ad_call_to_action_text: ad.demand_gen_multi_asset_ad.call_to_action_text,
+      demand_gen_multi_asset_ad_descriptions: ad.demand_gen_multi_asset_ad.descriptions?.map(description => description.text),
+      demand_gen_multi_asset_ad_headlines: ad.demand_gen_multi_asset_ad.headlines?.map(headline => headline.text),
+      demand_gen_multi_asset_ad_lead_form_only: ad.demand_gen_multi_asset_ad.lead_form_only,
+      demand_gen_multi_asset_ad_logo_images: ad.demand_gen_multi_asset_ad.logo_images?.map(image => image.asset),
+      demand_gen_multi_asset_ad_marketing_images: ad.demand_gen_multi_asset_ad.marketing_images?.map(image => image.asset),
 
-        // Video ad details
-        video_ad_bumper_action_button_label: ad.video_ad.bumper.action_button_label,
-        video_ad_bumper_action_headline: ad.video_ad.bumper.action_headline,
-        video_ad_in_feed_description1: ad.video_ad.in_feed.description1,
-        video_ad_in_feed_description2: ad.video_ad.in_feed.description2,
-        video_ad_in_feed_headline: ad.video_ad.in_feed.headline,
-        video_ad_in_feed_thumbnail: enums.VideoThumbnail[ad.video_ad.in_feed.thumbnail],
+      // Video ad details
+      video_ad_bumper_action_button_label: ad.video_ad.bumper.action_button_label,
+      video_ad_bumper_action_headline: ad.video_ad.bumper.action_headline,
+      video_ad_in_feed_description1: ad.video_ad.in_feed.description1,
+      video_ad_in_feed_description2: ad.video_ad.in_feed.description2,
+      video_ad_in_feed_headline: ad.video_ad.in_feed.headline,
+      video_ad_in_feed_thumbnail: enums.VideoThumbnail[ad.video_ad.in_feed.thumbnail],
 
-        // Responsive video ads
-        video_responsive_ad_breadcrumb1: ad.video_responsive_ad.breadcrumb1,
-        video_responsive_ad_breadcrumb2: ad.video_responsive_ad.breadcrumb2,
-        video_responsive_ad_call_to_actions: ad.video_responsive_ad.call_to_actions?.map(action => action.text),
-        video_responsive_ad_companion_banners: ad.video_responsive_ad.companion_banners?.map(banner => banner.asset),
+      // Responsive video ads
+      video_responsive_ad_breadcrumb1: ad.video_responsive_ad.breadcrumb1,
+      video_responsive_ad_breadcrumb2: ad.video_responsive_ad.breadcrumb2,
+      video_responsive_ad_call_to_actions: ad.video_responsive_ad.call_to_actions?.map(action => action.text),
+      video_responsive_ad_companion_banners: ad.video_responsive_ad.companion_banners?.map(banner => banner.asset),
 
-        // Other generic fields
-        ad_tracking_url_template: ad.tracking_url_template,
-      }));
+      // Other generic fields
+      ad_tracking_url_template: ad.tracking_url_template,
+    }));
 
-    } catch (error) {
-      throw new Error(`Google Ads API Error: ${JSON.stringify(error, null, 2)}`);
-    }
+    // } catch (error) {
+    //   throw new Error(`Google Ads API Error: ${JSON.stringify(error, null, 2)}`);
+    // }
   }
 
 }
