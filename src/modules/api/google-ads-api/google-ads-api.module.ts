88cf25dcb731e146cb47d6cfd3469c3f1c651a89
Roman <roman.senyshyn@surelockkey.com>
2024-12-18 13:20:31 +0200

    update Ad service

--- src/modules/api/google-ads-api/google-ads-api.module.ts
+++ src/modules/api/google-ads-api/google-ads-api.module.ts
@@ -3,9 +3,10 @@ import { GoogleAdsApiService } from './google-ads-api.service';
 import { GoogleAdsApiResolver } from './google-ads-api.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdCampaign } from './entity/ad-campaign.entity';
+import { AdGroup } from './entity/ads-group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdCampaign])],
+  imports: [TypeOrmModule.forFeature([AdCampaign, AdGroup])],
   providers: [GoogleAdsApiService, GoogleAdsApiResolver],
 })
-export class GoogleAdsApiModule {}
+export class GoogleAdsApiModule { }
