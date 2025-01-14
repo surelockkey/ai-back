16ad8a105fc36596527b5b22580cbc1340efa43f
Roman <roman.senyshyn@surelockkey.com>
2025-01-14 13:32:51 +0200

    update ADS service

--- src/modules/api/google-ads-api/google-ads-api.service.ts
+++ src/modules/api/google-ads-api/google-ads-api.service.ts
@@ -1341,8 +1341,9 @@ export class GoogleAdsApiService {
       const customer_ids = await this.getListCustomers();
 
       // for await (const id of customer_ids) {
+      const customer = this.createCustomer(process.env.GOOGLE_ADS_CUSTOMER_ID);
+
       while (date.isAfter(current_date)) {
-        const customer = this.createCustomer(process.env.GOOGLE_ADS_CUSTOMER_ID);
 
         const campaigns = await customer.query(campaign_query);
 
