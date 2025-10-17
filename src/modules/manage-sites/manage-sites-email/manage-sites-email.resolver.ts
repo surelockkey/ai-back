cab247d8421924f2f497a973b5cfb1356585277e
Nazarii Shparhalo <nazariishparhalo@Nazariis-MacBook-Pro.local>
2025-10-17 16:16:51 +0300

    add file upload to sending email with attachments

--- src/modules/manage-sites/manage-sites-email/manage-sites-email.resolver.ts
+++ src/modules/manage-sites/manage-sites-email/manage-sites-email.resolver.ts
@@ -1,20 +1,29 @@
 import { Resolver, Mutation, Args } from '@nestjs/graphql';
 import { ManageSitesEmailService } from './manage-sites-email.service';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
+import { FileUpload } from 'graphql-upload';
 
 @Resolver('ManageSitesEmail')
 export class ManageSitesEmailResolver {
   constructor(
     private readonly manageSitesEmailService: ManageSitesEmailService,
-  ) {}
+  ) { }
 
   @Mutation(() => Boolean)
   async sendManageSitesHtmlEmail(
     @Args('to') to: string,
     @Args('subject') subject: string,
     @Args('html') html: string,
+    @Args({ name: 'attachments', type: () => [GraphQLUpload], nullable: true })
+    attachments?: FileUpload[],
   ): Promise<boolean> {
     try {
-      await this.manageSitesEmailService.sendHtmlEmail(to, subject, html);
+      await this.manageSitesEmailService.sendHtmlEmail(
+        to,
+        subject,
+        html,
+        attachments,
+      );
       return true;
     } catch (error) {
       console.error('Error sending email:', error);
